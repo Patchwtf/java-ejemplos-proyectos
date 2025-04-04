0e5d5c41bb37f2a87e4d61257b53e6431c7c580b
Patchwtf <jhosbyndaniel@gmail.com>
2025-04-04 13:53:02 -0600

    Creacion de proyecto con las Bases. Creacion del primer objeto

---
+++ MyMedicalAppoitments/MyMedicalAppointments/src/Doctor.java
@@ -0,0 +1,12 @@
+public class Doctor {
+    //Propiedades
+    int id;
+    String name;
+    String speciality;
+
+    //Comportamientos - Metodos
+    
+    public void showName(){
+        System.out.println("Doctor name: " + name);
+    }
+}
