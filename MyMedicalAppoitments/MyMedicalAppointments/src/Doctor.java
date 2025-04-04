883c9ad84a43f671ff3815be04fe50003d273d56
Patchwtf <jhosbyndaniel@gmail.com>
2025-04-04 14:46:41 -0600

    IMpementacion del primer constructor

--- MyMedicalAppoitments/MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppoitments/MyMedicalAppointments/src/Doctor.java
@@ -1,12 +1,19 @@
+
 public class Doctor {
+
     //Propiedades
     int id;
     String name;
     String speciality;
 
+    //Constructor
+    Doctor(String name, String speciality) {
+        this.name = name;
+        this.speciality = speciality;
+    }
+
     //Comportamientos - Metodos
-    
-    public void showName(){
+    public void showName() {
         System.out.println("Doctor name: " + name);
     }
 }
--- MyMedicalAppoitments/MyMedicalAppointments/src/Main.java
+++ MyMedicalAppoitments/MyMedicalAppointments/src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Ana Linderman", "neuro");
         myDoctor.name = "John Doe";
         myDoctor.showName();
+        
         
     }
 }
