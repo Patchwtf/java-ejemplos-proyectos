ec78e36404a4d4f7182dc24291db4ef18ea3704a
Jhosbyn Daniel <jhosbyndaniel@gmail.com>
2025-04-06 01:01:48 -0600

    implementar getter y setters

--- MyMedicalAppoitments/MyMedicalAppointments/src/Patient.java
+++ MyMedicalAppoitments/MyMedicalAppointments/src/Patient.java
@@ -14,5 +14,68 @@ public class Patient {
         this.name = name;
         this.email = email;
     }
-    
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getWeight() {
+        return this.weight + " kg";
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getHeight() {
+        return this.height + " mts";
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
+    public String getBlood() {
+        return this.blood;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getAddress() {
+        return this.address;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        if(phoneNumber.length() == 10){
+            this.phoneNumber = phoneNumber;
+        }else{
+            System.out.println("El telefono no es valido");
+        }
+    }
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+    public void setBirthday(String birthday) {
+        
+        this.birthday = birthday;
+    }   
+    public String getBirthday() {
+        return this.birthday;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 }
