195fa6b7ce8fc08eafa0e0be769245601c875481
Jhosbyn Daniel <jhosbyndaniel@gmail.com>
2025-04-05 20:55:53 -0600

    Implementacion de Paquetes, estatics y finals

--- MyMedicalAppoitments/MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppoitments/MyMedicalAppointments/src/Doctor.java
@@ -2,18 +2,27 @@
 public class Doctor {
 
     //Propiedades
-    int id;
+    static int id = 0; //autoincrement
     String name;
     String speciality;
 
     //Constructor
-    Doctor(String name, String speciality) {
-        this.name = name;
-        this.speciality = speciality;
+    // Doctor(String name, String speciality) {
+    //     this.name = name;
+    //     this.speciality = speciality;
+    // }
+
+    Doctor(){
+        System.out.println("Contructor Doctor() called");
+        id++;
     }
 
     //Comportamientos - Metodos
     public void showName() {
         System.out.println("Doctor name: " + name);
+    }
+
+    public void showId() {
+        System.out.println("Doctor id: " + id);
     }
 }
--- MyMedicalAppoitments/MyMedicalAppointments/src/Main.java
+++ MyMedicalAppoitments/MyMedicalAppointments/src/Main.java
@@ -1,9 +1,16 @@
+import static ui.UIMenu.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("Ana Linderman", "neuro");
-        myDoctor.name = "John Doe";
-        myDoctor.showName();
-        
-        
+        // Doctor myDoctor = new Doctor();
+        // myDoctor.name = "John Doe";
+        // myDoctor.showName();
+        // myDoctor.showId();
+
+        // Doctor myDoctorAnn = new Doctor();
+        // myDoctorAnn.showId();
+
+        showMenu();
     }
 }
