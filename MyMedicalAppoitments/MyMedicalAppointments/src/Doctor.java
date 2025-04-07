6249bc9a26e14c050f8e544940b1134f0281bdcd
Jhosbyn Daniel <jhosbyndaniel@gmail.com>
2025-04-06 21:19:49 -0600

    Implementacion de clases anidadas

--- MyMedicalAppoitments/MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppoitments/MyMedicalAppointments/src/Doctor.java
@@ -1,21 +1,23 @@
 
+import java.util.*;
+
 public class Doctor {
 
     //Propiedades
     static int id = 0; //autoincrement
-    String name;
-    String email;
-    String speciality;
+    private String name;
+    private String email;
+    private String speciality;
 
     //Constructor
     // Doctor(String name, String speciality) {
     //     this.name = name;
     //     this.speciality = speciality;
     // }
-
-    Doctor(){
+    Doctor() {
         System.out.println("Contructor Doctor() called");
-            }
+    }
+
     Doctor(String name, String speciality) {
         System.out.println("El nombre asignado al doctor es: " + name);
         this.name = name;
@@ -30,5 +32,52 @@ public class Doctor {
 
     public void showId() {
         System.out.println("Doctor id: " + id);
+    }
+
+    public ArrayList<AvaibleAppointments> getAvailableAppointments() {
+        return availableAppointments;
+    }
+
+    ArrayList<AvaibleAppointments> availableAppointments = new ArrayList<>();
+
+    public void addAvailableAppointment(Date date, String time) {
+        availableAppointments.add(new AvaibleAppointments(date, time));
+    }
+
+    public static class AvaibleAppointments {
+
+        private int id;
+        private Date date;
+        private String time;
+
+        //Avaible appointment
+        public AvaibleAppointments(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
--- MyMedicalAppoitments/MyMedicalAppointments/src/Main.java
+++ MyMedicalAppoitments/MyMedicalAppointments/src/Main.java
@@ -1,7 +1,9 @@
-import static ui.UIMenu.*;
+//import static ui.UIMenu.*;
 
+import java.util.Date;
 
 public class Main {
+
     public static void main(String[] args) {
         // Doctor myDoctor = new Doctor();
         // myDoctor.name = "John Doe";
@@ -10,19 +12,27 @@ public class Main {
 
         // Doctor myDoctorAnn = new Doctor();
         // myDoctorAnn.showId();
-        
         //?Implementacion de menu
         //showMenu();
-
         //*implementacion de sobre carga y pasarle variables al instancias objetos */
         Doctor myDoctor = new Doctor("Parche", "Neuro");
-        System.err.println(myDoctor.name + "\t" + myDoctor.speciality);
+        myDoctor.addAvailableAppointment(new Date(), "4 pm");
+        myDoctor.addAvailableAppointment(new Date(), "10 pm");
+        myDoctor.addAvailableAppointment(new Date(), "1 pm");
 
+        System.out.println(myDoctor.getAvailableAppointments());
+
+        for (Doctor.AvaibleAppointments avaibleAppointments : myDoctor.getAvailableAppointments()) {
+            System.out.println(avaibleAppointments.getDate() + " " + avaibleAppointments.getTime());
+        }
+
+        //System.err.println(myDoctor.name + "\t" + myDoctor.speciality);
         Patient patient = new Patient("Alex", "nobody@mail.com");
         patient.setWeight(70.0);
         System.err.println(patient.getWeight());
 
         patient.setPhoneNumber("1234567890");
         System.err.println(patient.getPhoneNumber());
+
     }
 }
