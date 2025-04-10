508e230076ff1caa7337edf488ef29af24358056
Patchwtf <jhosbyndaniel@gmail.com>
2025-04-10 09:27:09 -0600

    Implementacion de simulacion de autenticacion

--- MyMedicalAppoitments/MyMedicalAppointments/src/ui/UIMenu.java
+++ MyMedicalAppoitments/MyMedicalAppointments/src/ui/UIMenu.java
@@ -1,12 +1,20 @@
 package ui;
+
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import model.Doctor;
+import model.Patient;
 
 public class UIMenu {
 
-    public static final String [] Months = {"January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December"};
+    public static final String[] Months = {"January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"};
 
-    public static void showMenu(){
+    public static Doctor doctorLogged;
+    public static Patient patienLogged;
+
+    public static void showMenu() {
         System.out.println("Welcome to My Appointments");
         System.out.println("Selecciona la opción deseada");
 
@@ -19,13 +27,15 @@ public class UIMenu {
             Scanner sc = new Scanner(System.in);
             response = Integer.valueOf(sc.nextLine());
 
-            switch (response){
+            switch (response) {
                 case 1:
+                    response = 0;
                     System.out.println("Doctor");
+                    authUser(1);
                     break;
                 case 2:
                     response = 0;
-                    showPatientMenu();
+                    authUser(2);
 
                     break;
                 case 0:
@@ -34,10 +44,49 @@ public class UIMenu {
                 default:
                     System.out.println("Please select a correct answer");
             }
-        }while (response != 0);
+        } while (response != 0);
     }
 
-    static void showPatientMenu(){
+    private static void authUser(int userType) {
+        ArrayList<Doctor> doctors = new ArrayList<>();
+        doctors.add(new Doctor("Alejandro Martinez", "alejandro@mail.com", null));
+        doctors.add(new Doctor("Rocio Hernandez", "rocio@mail.com", null));
+        doctors.add(new Doctor("Juan Perez", "juan@mail.com", null));
+
+        ArrayList<Patient> pacientes = new ArrayList<>();
+        pacientes.add(new Patient("Juan Barrera", "barrera@gmail"));
+        pacientes.add(new Patient("Daniel Salgado", "daniel@gmail"));
+        pacientes.add(new Patient("Roberto Martinez", "roberto@gmail"));
+
+        boolean emailCorrect = false;
+
+        do {
+            System.out.println("Insert your email: [x@mail.com]");
+            Scanner sc = new Scanner(System.in);
+            String email = sc.nextLine();
+
+            if (userType == 1) {
+                for (Doctor d : doctors) {
+                    if (d.getEmail().equals(email)) {
+                        emailCorrect = true;
+                        doctorLogged = d;
+
+                    }
+                }
+            } else if (userType == 2) {
+                for (Patient d : pacientes) {
+                    if (d.getEmail().equals(email)) {
+                        emailCorrect = true;
+                        patienLogged = d;
+
+                    }
+                }
+            }
+
+        } while (!emailCorrect);
+    }
+
+    static void showPatientMenu() {
         int response = 0;
         do {
             System.out.println("\n\n");
@@ -49,10 +98,10 @@ public class UIMenu {
             Scanner sc = new Scanner(System.in);
             response = Integer.valueOf(sc.nextLine());
 
-            switch (response){
+            switch (response) {
                 case 1:
                     System.out.println("::Book an appointment");
-                    for (int i = 0; i < 4; i++){
+                    for (int i = 0; i < 4; i++) {
                         System.out.println(i + ". " + Months[i]);
                     }
                     break;
@@ -63,6 +112,6 @@ public class UIMenu {
                     showMenu();
                     break;
             }
-        }while (response != 0);
+        } while (response != 0);
     }
 }
