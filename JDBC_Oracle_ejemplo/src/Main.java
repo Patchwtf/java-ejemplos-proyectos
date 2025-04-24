7a2af855a799c02f60f7d0a78b0be1751177e3a8
Patchwtf <jhosbyndaniel@gmail.com>
2025-04-24 12:21:46 -0600

    Se configuró conector, Instalar imagen de Docker

--- JDBC_Oracle_ejemplo/src/Main.java
+++ JDBC_Oracle_ejemplo/src/Main.java
@@ -1,5 +1,27 @@
+import java.sql.*;
+
+//Descargar Imagen de MySQL de Docker
+//docker pull mysql:9.3.0
+//
+//Para crear la Base, seguir el archivo ./conector/creacion_base.txt
+
 public class Main {
     public static void main(String[] args) {
+
+        Connection myConn = null;
+        Statement myStamt = null;
+        ResultSet myRes = null;
+
+        try{
+            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "1234");
+            System.out.println("Genial, nos conectamos");
+
+            myStamt = myConn.createStatement();
+            myRes = myStamt.executeQuery("SELECT * FROM EMPLOYEES");
+        } catch (SQLException e){
+
+            System.out.println("Ni modos, algo salió mal \n" + e.getLocalizedMessage() + " | Error Code:" + e.getErrorCode());
+        }
 
     }
 }
