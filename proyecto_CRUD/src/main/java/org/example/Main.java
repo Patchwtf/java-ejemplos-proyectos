f97f141cbf491ef4e3adc23c2e7b916bd5c07565
Patchwtf <jhosbyndaniel@gmail.com>
2025-05-02 15:08:15 -0600

    Implementacion de vista en Main

---
+++ proyecto_CRUD/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.example.view.SwingApp;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args){
+        SwingApp app = new SwingApp();
+        app.setVisible(true);
+    }
+}
