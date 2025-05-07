86c4efc06a1ae7723a46b32c175d7f31615862bf
Patchwtf <jhosbyndaniel@gmail.com>
2025-05-07 16:16:30 -0600

    Implementacion de creacion de mensajes

--- proyectoPersistencia/mensajes_app/src/main/java/org/example/MensajeService.java
+++ proyectoPersistencia/mensajes_app/src/main/java/org/example/MensajeService.java
@@ -1,8 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class MensajeService {
     public static void crearMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Cual mensaje quieres escribir?");
+        String mensajeText = sc.nextLine();
+        System.out.println("Tu nombre:");
+        String nombre = sc.nextLine();
 
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensajeText);
+        registro.setAutor_mensaje(nombre);
+
+        MensajesDAO.crearMensajeBD(registro);
     }
     public static void listarMensajes(){
 
--- proyectoPersistencia/mensajes_app/src/main/java/org/example/MensajesDAO.java
+++ proyectoPersistencia/mensajes_app/src/main/java/org/example/MensajesDAO.java
@@ -7,9 +7,13 @@ public class MensajesDAO {
     public static void crearMensajeBD(Mensajes mensaje){
         Conexion db_connect = new Conexion();
         try(Connection cnx = db_connect.get_connection();
-            PreparedStatement myStamt = cnx.prepareStatement("INSERT INTO employees (mensaje, autor_mensaje) VALUES (?, ?);");){
-
+            PreparedStatement myStamt = cnx.prepareStatement("INSERT INTO mensajes (mensaje, autor_mensaje) VALUES (?, ?)");){
+            myStamt.setString(1, mensaje.getMensaje());
+            myStamt.setString(2, mensaje.getAutor_mensaje());
+            myStamt.executeUpdate();
+            System.out.println("El mensaje se creó correctamente");
         } catch (Exception e) {
+            System.out.println("El mensaje no se logró crear :(\n");
             throw new RuntimeException(e);
         }
     }
