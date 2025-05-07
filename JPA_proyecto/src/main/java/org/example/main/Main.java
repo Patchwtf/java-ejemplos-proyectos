88463abc60c9f71722715c23e11e627f9286e1a8
Jhosbyn Daniel <jhosbyndaniel@gmail.com>
2025-05-06 22:51:51 -0600

    Se arregla problemas en dependencias de hibernate

--- JPA_proyecto/src/main/java/org/example/main/Main.java
+++ JPA_proyecto/src/main/java/org/example/main/Main.java
@@ -1,7 +1,16 @@
 package org.example.main;
 
+import org.example.Util.UtilEntity;
+import org.example.entity.Employee;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        EntityManager em = UtilEntity.getEntityManager();
+        List<Employee> employees = em.createQuery("SELECT e FROM Employee e", Employee.class).getResultList();
+        System.out.println("-------------------Listar Todos----------------");
+        employees.forEach(System.out::println);
     }
 }
