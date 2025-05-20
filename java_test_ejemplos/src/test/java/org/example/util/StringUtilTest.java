b582581e1d68333e27e74c22cbe8182b9709a2a0
Jhosbyn Daniel <jhosbyndaniel@gmail.com>
2025-05-19 22:22:52 -0600

    Primer prueba manual

---
+++ java_test_ejemplos/src/test/java/org/example/util/StringUtilTest.java
@@ -0,0 +1,8 @@
+package org.example.util;
+
+class StringUtilTest {
+    public static void main(String[] args) {
+        String result = StringUtil.repeat("hola", 3);
+        System.out.println(result);
+    }
+}
