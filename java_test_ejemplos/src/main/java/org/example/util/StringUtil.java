fbf8f374bf34c0c0845298c94bd995d0fc4da34b
Jhosbyn Daniel <jhosbyndaniel@gmail.com>
2025-05-20 22:08:15 -0600

    Implementacion de JUnit, Se añade ejemplos de test con JUnit

--- java_test_ejemplos/src/main/java/org/example/util/StringUtil.java
+++ java_test_ejemplos/src/main/java/org/example/util/StringUtil.java
@@ -2,6 +2,9 @@ package org.example.util;
 
 public class StringUtil {
     public static String repeat(String str, int times){
+
+        if (times < 0) throw new IllegalArgumentException("Cantidad negativa");
+
         String result = "";
         for (int i = 0; i < times; i++) {
             result += str;
--- java_test_ejemplos/src/test/java/org/example/util/StringUtilTest.java
+++ java_test_ejemplos/src/test/java/org/example/util/StringUtilTest.java
@@ -1,11 +1,23 @@
 package org.example.util;
 
-class StringUtilTest {
-    public static void main(String[] args) {
-        assertEquals(StringUtil.repeat("hola", 3), "holaholahol");
-    }
+import org.junit.Assert;
+import org.junit.Test;
 
-    private static void assertEquals(String result, String expected) {
-        if(!result.equals(expected)) throw new RuntimeException(result + " is not equal to " + expected);
+public class StringUtilTest {
+    @Test
+    public void testRepeatOnce() {
+        Assert.assertEquals( "hola", StringUtil.repeat("hola", 1));
+    }
+    @Test
+    public void testRepeatMultipleTimes() {
+        Assert.assertEquals( "holaholaholaholahola", StringUtil.repeat("hola", 5));
+    }
+    @Test
+    public void testRepeatZero() {
+        Assert.assertEquals( "", StringUtil.repeat("hola", 0));
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testRepeatNegative() {
+        StringUtil.repeat("hola", -1);
     }
 }
