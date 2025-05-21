27774916cc7094fe9cef28cb2a12f97256081422
Jhosbyn Daniel <jhosbyndaniel@gmail.com>
2025-05-20 22:55:48 -0600

    Implementacionde pssword

--- java_test_ejemplos/src/main/java/org/example/util/PasswordUtil.java
+++ java_test_ejemplos/src/main/java/org/example/util/PasswordUtil.java
@@ -7,7 +7,7 @@ public class PasswordUtil {
     }
 
     public static SecurityLevel assessPassword(String password){
-        if(password.matches("[a-zA-Z]+||.{0,7}"))return SecurityLevel.WEAK;
+        if(password.matches("[a-zA-Z]+|.{0,7}"))return SecurityLevel.WEAK;
         if(password.matches("(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$")) return SecurityLevel.MEDIUM;
         if(password.matches("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\\W).{8,}$")) return SecurityLevel.STRONG;
         return SecurityLevel.WEAK;
